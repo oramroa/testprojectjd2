eedf28ea2975e7295b475aa65ccfbf5a240c718a
sava-s <turbocha@bk.ru>
2020-11-04 11:24:05 +0300

    Cherry pick

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World1111111");
+        System.out.println("String for cherry pick");
     }
 }
