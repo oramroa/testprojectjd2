5478ce5c9bc5cc1fbaff57345d5e0d74c7e80538
sava-s <turbocha@bk.ru>
2020-11-04 11:15:29 +0300

    new commit

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -3,5 +3,6 @@ package com.noirix;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World1111111");
     }
 }
