4ebb812ebabdd103dc3e90454d5e8f364e280522
sava-s <turbocha@bk.ru>
2020-11-08 22:07:25 +0300

    Charset and Auth token in Header filters added

---
+++ src/main/java/com/noirix/filter/CharsetFilter.java
@@ -0,0 +1,26 @@
+package com.noirix.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CharsetFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        System.out.println("In Charset filter");
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        chain.doFilter(request, response);
+    }
+}
